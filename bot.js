38a9361433ef7799a4f7b2b3cc934e96d3b87d93
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-02-14 21:26:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,17 @@ const bot = new Discord.Client();
 
 bot.on('ready', () => {
   console.log('I am ready!');
-   bot.user.setUsername("Bot Dude");
-});
-
-bot.on("disconnected", function () {
-	console.log("Disconnected!");
-	process.exit(1);
+  bot.user.setUsername("Bot Dude");
 });
 
 bot.on('message', message => {
-    if (message.content === "!restrict") {
-      message.reply("wat");
+  if (message.content.startsWith("!restrict")) {
+    message.channel.send("wat");
     // message.author.voiceChannel.setUserLimit();
-    }
-    if (message.content === 'ping') {
-      bot.sendMessage(message.channel, 'pong');
-    }
+  }
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
