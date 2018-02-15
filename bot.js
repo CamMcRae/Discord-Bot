5ccbd659a8d6f2e918502a73f6f5185fd08f2441
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-02-15 00:06:11 -0500

    asd

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ bot.on('message', message => {
   } else if (message.content === 'pong') {
     message.channel.send('hah you suck');
   } else if (message.content.startsWith(prefix + "lmgtfy")) {
-    message.channel.send("http://lmgtfy.com/?q=" + message.content.substr(8).replace(" ", "%20"));
+    message.channel.send("http://lmgtfy.com/?q=" + message.content.substr(8).replace(/ /g, 'hi', "%20"));
   }
 });
 //asdas
