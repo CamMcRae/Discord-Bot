6be935359d0aa13db95ede6ecdc1701bdb23905d
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-02-22 01:01:39 -0500

    cleaned up

--- bot.js
+++ bot.js
@@ -16,74 +16,50 @@ bot.on('message', message => {
   if (message.author.bot) return;
   const query = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = query.shift().toLowerCase();
-  switch (command) {
-    case "restrict":
-      message.channel.send('wat');
-      break;
-    case "lmgtfy":
-      message.channel.send("http://lmgtfy.com/?q=" + message.content.substr(8).replace(/ /g, "%20"));
-      break;
-    case "define":
-      let searchQuery = query.join(" ").toLowerCase();
-      let url = `https://www.dictionaryapi.com/api/v1/references/collegiate/xml/${searchQuery}?key=${dictKey}`;
-      apiRequest(url, "dict", message, dictionary);
-      break;
-    case "this bot sucks":
-      message.channel.send("No it doesn't");
-      break;
-    case "link":
-      mainChannel = message.channel;
-    case "prefix":
-      if (message.author.hasPermission("ADMINISTRATOR") && query.length == 1) {
-        prefix = query;
-      }
-      break;
+  if (message.content.startsWith($)) {
+    switch (command) {
+      case "restrict":
+        message.channel.send('wat');
+        break;
+      case "lmgtfy":
+        message.channel.send("http://lmgtfy.com/?q=" + message.content.substr(8).replace(/ /g, "%20"));
+        break;
+      case "define":
+        let searchQuery = query.join(" ").toLowerCase();
+        let url = `https://www.dictionaryapi.com/api/v1/references/collegiate/xml/${searchQuery}?key=${dictKey}`;
+        apiRequest(url, "dict", message, dictionary);
+        break;
+      case "thesaurus":
+        let searchQuery = query.join(" ").toLowerCase();
+        // thesaurus
+        break;
+      case "this bot sucks":
+        message.channel.send("No it doesn't");
+        break;
+      case "link":
+        mainChannel = message.channel;
+      case "prefix":
+        if (message.author.hasPermission("ADMINISTRATOR") && query.length == 1) {
+          prefix = query;
+        }
+        break;
+      case "clean":
+        // go up through bot messages and delete them until 1 day old
+        break;
+    }
+  } else {
+    switch (message.content) {
+      case "ping":
+        message.channel.send('pong');
+      case "pong":
+        message.channel.send('hah you suck');
+    }
   }
-  switch (message.content) {
-    case "ping":
-      message.channel.send('pong');
-    case "pong":
-      message.channel.send('hah you suck');
-  }
-  // if (command) {
-  //   if (message.content.startsWith(prefix + "restrict") && message.auther.hasPermission("ADMINISTRATOR")) {
-  //     message.channel.send('wat');
-  //     // message.author.voiceChannel.setUserLimit(message.content.substr(10));
-  //   } else if (message.content.startsWith(prefix + "lmgtfy")) {
-  //     message.channel.send("http://lmgtfy.com/?q=" + message.content.substr(8).replace(/ /g, "%20"));
-  //   } else if (message.content.startsWith(prefix + "define")) {
-  //     let query = message.content.substr(8).trim().replace(/ /g, "_").toLowerCase();
-  //     let url = `https://www.dictionaryapi.com/api/v1/references/collegiate/xml/${query}?key=${dictKey}`;
-  //     apiRequest(url, "dict", message, dictionary);
-  //   } else if (message.content.startsWith(prefix + "this bot sucks")) {
-  //     message.channel.send("No it doesn't");
-  //   } else if (message.content.startsWith(prefix + "link")) {
-  //     mainChannel = message.channel;
-  //   } else if (message.content.startsWith(prefix + "prefix")) {
-  //     if (message.author.hasPermission("ADMINISTRATOR") && message.content.substr(7).trim().length == 1) {
-  //       prefix = message.content.substr(7).trim();
-  //     }
-  //   }
-  // } else {
-  //   if (message.content === 'ping') {
-  //     message.channel.send('pong');
-  //   } else if (message.content === 'pong') {
-  //     message.channel.send('hah you suck');
-  //   }
-  // }
-  //else if (message.content.startsWith(prefix + "thesaurus")) {
-  //   let query = message.content.substr(8, len(message.content.trim() - 2)).trim().replace(/ /g, "_").lowercased();
-  // } else if (message.content.startsWith(prefix + "clean")) {
-  //   while (true) {
-  //     //go up through bot messages and delete them until 1 day old
-  //     break;
-  //   }
 });
 
 bot.login(process.env.BOT_TOKEN);
 
 function dictionary(json, type, message) {
-  message.channel.send(json);
   let entries = [];
   for (let i in json.entry) {
     for (let j of json.entry[i].def) {
@@ -102,7 +78,6 @@ function dictionary(json, type, message) {
   let embed = printMsg(entries, json, type);
   message.channel.send(embed);
 }
-
 
 // goes through json for dictionary entries
 function printMsg(entries, json, type) {
@@ -164,17 +139,6 @@ function apiRequest(url, type, message, callback) {
     });
   });
 }
-//   let xhttp = new XMLHttpRequest(); // opens html request
-//   xhttp.onreadystatechange = function() { // waits until an xml is returned
-//     if (this.readyState == 4 && this.status == 200) {
-//       let json = getJSON(this);
-//       console.log("got json");
-//       callback(json, type);
-//     }
-//   };
-//   xhttp.open("GET", url, true); // opens request
-//   xhttp.send(); //starts request
-// }
 
 function getJSON(xml, type, message, callback) {
   let parser = new xml2js.Parser();
