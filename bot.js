18a5f9f01a51469a47be3f0bebbb94ff453ab0e0
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-03-07 22:02:04 -0500

    sdfsdf

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
 const xml2js = require('xml2js');
+const Discord = require('discord.js');
 const https = require('https');
 const fs = require("fs");
 const bot = new Discord.Client();
