10f4bfcd731e7fecf7b9a2a6dc3dc7bc828f2ff8
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-03-11 03:17:45 -0400

    dsf

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+
+// const Discord = require("discord.js");
 // const xml2js = require("xml2js");
 const fs = require("fs");
 const bot = new Discord.Client();
