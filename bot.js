e76ef5549ba4c53bb487985c3d8e6cd70f4c052b
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-03-12 23:06:54 -0400

    sdf

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const words = ["fak", "fuck", "shit"];
 
 bot.on('ready', () => {
   console.log('I am ready!');
-  bot.user.setActivity('You', { type: 'WATCHING' })
+  bot.user.setActivity('a game', { type: 'PLAYING' })
   // bot.user.setUsername("Bot Dude");
 });
 
