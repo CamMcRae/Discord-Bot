1635a7ac705b3d20e3da2a99520ab476ad9d35f3
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-03-17 00:01:48 -0400

    fdgdf

--- bot.js
+++ bot.js
@@ -22,7 +22,8 @@ const commands = [
   ["Wikipedia Page", config.prefix + "wiki <query>"],
   ["Coinflip", config.prefix + "coinflip", config.prefix + "flipacoin"],
   ["Dice Roll", config.prefix + "roll <number of dice> <amount of sides>"],
-  ["Google Search", config.prefix + "google <query>", config.prefix + "whatis <query>"]
+  ["Google Search", config.prefix + "google <query>", config.prefix + "whatis <query>"],
+  ["Lunch Menu", config.prefix + "lunch", config.prefix + "lunch <yesterday/today/tomorrow>", config.prefix + "lunch <day> <month> <year>"]
 ]; //[[Description, syntax1, syntax2, etc],...]
 
 
