f3f8a83e472f1aa1068a588d92988dd28fff5e83
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-03-27 01:40:11 -0400

    sdf

--- other.js
+++ other.js
@@ -1,9 +1,12 @@
 module.exports.coinflip = (query) => {
-  if (query.length > 0) {
-    return ("The coin landed on " + (Math.random() >= 0.5 ? query[0] : query[1]) + "!").catch((err) => {
+  switch (query.length) {
+    case 2:
+      return "The coin landed on " + (Math.random() >= 0.5 ? query[0] : query[1]) + "!";
+      break;
+    case 0:
+      return "The coin landed on " + (Math.random() >= 0.5 ? "heads!" : "tails!");
+      break
+    default:
       return "Invalid Arguments!"
-    });
-  } else {
-    return "The coin landed on " + (Math.random() >= 0.5 ? "heads!" : "tails!");
   }
 }
