5dd09dca4366e9d6539850d6fc3f79b8890282a1
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-03-27 01:43:38 -0400

    hgj

--- bot.js
+++ bot.js
@@ -204,21 +204,7 @@ bot.on('message', message => {
         message.channel.send(other.coinflip(query));
         break;
       case "roll":
-        let rolls = [];
-        let diceAmt;
-        try {
-          diceAmt = parseInt(query.shift());
-        } catch (e) {
-          message.channel.send("Invalid arguments");
-          diceAmt = 0;
-        }
-        let max = parseInt(query.shift()) || 6;
-        for (let i = 0; i < diceAmt; i++) {
-          rolls.push(Math.floor(Math.random() * Math.floor(max) + 1));
-        }
-        if (rolls.length > 0) {
-          message.channel.send("The dice landed on: " + rolls.join(", ") + " with a total sum of " + rolls.reduce((a, b) => a + b, 0)).catch("Ya dun did something and it no work.");
-        }
+        other.rollDice(query, message);
         break;
         // case "google":
         // case "whatis":
--- other.js
+++ other.js
@@ -10,3 +10,21 @@ module.exports.coinflip = (query) => {
       return "Invalid Arguments!"
   }
 }
+
+module.exports.rollDice = (query, message) => {
+  let rolls = [];
+  let diceAmt;
+  try {
+    diceAmt = parseInt(query.shift());
+  } catch (e) {
+    message.channel.send("Invalid arguments");
+    diceAmt = 0;
+  }
+  let max = parseInt(query.shift()) || 6;
+  for (let i = 0; i < diceAmt; i++) {
+    rolls.push(Math.floor(Math.random() * Math.floor(max) + 1));
+  }
+  if (rolls.length > 0) {
+    message.channel.send("The dice landed on: " + rolls.join(", ") + " with a total sum of " + rolls.reduce((a, b) => a + b, 0)).catch("Ya dun did something and it no work.");
+  }
+}
