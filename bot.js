c1d6e0b2c2fb2b300f017706eaab46d1efa9f663
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-04-18 23:58:22 -0400

    sdf

--- bot.js
+++ bot.js
@@ -186,7 +186,7 @@ bot.on('message', message => {
           message.channel.send(":x: Mention who you want to move.\n - " + commands.move.usage.join("\n - "));
           break;
         }
-        console.log(message.mentions.users);
+        console.log(message.mentions.users.includes(message.member.id));
         if (message.mentions.users.find(user => user.id == message.member.id)) {
           console.log("this thing");
         }
