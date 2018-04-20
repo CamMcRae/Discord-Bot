0145c106551a008b6c4b220ac00d56e38dff1b62
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-04-19 22:50:01 -0400

    sdf

--- bot.js
+++ bot.js
@@ -186,8 +186,10 @@ bot.on('message', message => {
           message.channel.send(":x: Mention who you want to move.\n - " + commands.move.usage.join("\n - "));
           break;
         }
-        if (!message.mentions.users.find(user => user.id == message.member.id && !message.member.roles.find("name", config.adminRole))) {
-          break;
+        if (!message.member.roles.find("name", config.adminRole)) {
+          if (!message.mentions.users.find(user => user.id == message.member.id)) {
+            break;
+          }
         }
         if (!message.member.voiceChannel) {
           message.channel.send(":x: You need to be in a voice channel to use this command.");
