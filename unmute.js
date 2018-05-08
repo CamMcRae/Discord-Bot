246c925ad21319251d51a3186dc09a152d43ce4e
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-05-07 22:23:12 -0400

    dfg

--- unmute.js
+++ unmute.js
@@ -1,8 +1,7 @@
 module.exports.run = (client, message, query) => {
-  console.log(query);
-  if (!query) query = false;
+  if (!query.length) query = false;
   if (message.member.hasPermission("MUTE_MEMBERS")) {
-    message.guild.members.forEach(m => m.setMute(query));
+    message.guild.members.forEach(m => m.setMute(query[0]));
   } else {
     message.guild.members.filter(m => m.id === client.user.id).forEach(m => m.setMute(false));
   }
