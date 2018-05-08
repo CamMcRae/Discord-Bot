b97e349e63c1706e07d2f3ae65386c2593615c57
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-05-07 22:18:36 -0400

    dfg

--- unmute.js
+++ unmute.js
@@ -1,4 +1,5 @@
 module.exports.run = (client, message, query) => {
+  console.log(query);
   if (!query) query = false;
   if (message.member.hasPermission("MUTE_MEMBERS")) {
     message.guild.members.forEach(m => m.setMute(query));
