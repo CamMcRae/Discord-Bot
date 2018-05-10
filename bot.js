8faec5d9827c7ba020170ae88baf66f6b2aeda02
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-05-09 21:29:46 -0400

    gdfg

--- bot.js
+++ bot.js
@@ -76,12 +76,15 @@ bot.login(process.env.BOT_TOKEN);
 bot.on('message', message => {
   // deletes any messages starting with !
   if (message.content.slice(0, 1) == "!") {
-    message.delete();
     const channel = message.guild.channels.find(channel => channel.name === "music").toString();
+    if (message.channel.id == channel.id) return;
+    message.delete();
     message.channel.send("Use " + channel + " please! :angry:");
   }
   // deletes any bot messages
   if (message.author.bot) {
+    const channel = message.guild.channels.find(channel => channel.name === "music").toString();
+    if (message.channel.id == channel.id) return;
     if (message.author.id != bot.user.id) {
       message.delete();
     }
