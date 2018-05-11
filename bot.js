0ec6a0537ca627c8a8a02a507a762b0a969e4736
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-05-10 23:06:18 -0400

    dfg

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ bot.on('message', message => {
     const channel = message.guild.channels.find(channel => channel.name === "music");
     if (message.channel == channel) return;
     message.delete();
-    message.channel.send("Use " + channel.toString() + " please! :angry:");
+    message.channel.send(message.author + "Use " + channel.toString() + " please! :angry:");
   }
 
   // deletes any bot messages
