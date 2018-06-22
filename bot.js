83cb5325e6b0e2ca106deaee0541fd7a99d0e5e9
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-06-21 21:06:01 -0400

    sdf

--- bot.js
+++ bot.js
@@ -17,8 +17,8 @@ bot.login(process.env.BOT_TOKEN);
 
 bot.on('message', message => {
   // if a bot is talking or not a server
-  if (message.author.bot) return;
-  if (!message.guild) return;
+  // if (message.author.bot) return;
+  // if (!message.guild) return;
 
   // subreddit
   if (message.content.startsWith("r/")) {
