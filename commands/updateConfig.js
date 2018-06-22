dac2d8845d19f5d5c83193e95bc91f9445c6e7dd
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-06-22 18:50:12 -0400

    sdf

--- commands/updateConfig.js
+++ commands/updateConfig.js
@@ -5,7 +5,7 @@ if (process.env.REDISTOGO_URL) {
   redis.auth(rtg.auth.split(":")[1]);
 }
 
-module.exports.run = (id, key, value) = {
+module.exports.run = (id, key, value) => {
   redis.get((id).toString(), (err, config) => {
     redis.set((id).toString(), updateConfig(JSON.parse(config), (key).toString(), (value).toString()));
   });
