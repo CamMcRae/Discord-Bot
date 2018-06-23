0ab73bf773a5b317d9d165741b68e022f5c4a0ff
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-06-23 01:13:13 -0400

    Updated Coinflip

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,14 +1,9 @@
 // pre:
 // post:
 module.exports.run = (client, message, query) => {
-  switch (query.length) {
-    case 2:
-      message.channel.send("The coin landed on " + (Math.random() >= 0.5 ? query[0] : query[1]) + "!");
-      break;
-    case 0:
-      message.channel.send("The coin landed on " + (Math.random() >= 0.5 ? "heads!" : "tails!"));
-      break
-    default:
-      return "Invalid Arguments!"
+  if (query.length > 0) {
+    message.channel.send("The coin landed on " + query[Math.floor(Math.random() * query.length)] + "!");
+  } else {
+    message.channel.send("The coin landed on " + (Math.random() >= 0.5 ? "heads!" : "tails!"));
   }
 }
