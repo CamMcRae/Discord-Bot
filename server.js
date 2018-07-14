cc9ac2789ede825e64cf32e17eb1352429605dfe
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-07-14 04:17:40 -0400

    sdf

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const sio = require('socket.io')
 const server = express.listen(process.env.PORT);
 const io = sio(server);
 
-
 io.on('connect', (socket) => {
   console.log('Client connected');
-  io.sockets.emit('test', "The quick brown fox");
+  socket.on("getMenu", (d) => console.log(d));
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
