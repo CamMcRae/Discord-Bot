5bdfe322e698750337b752d01eb1d42bbb0b8b5d
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-07-14 02:14:07 -0400

    dfg

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')();
 const sio = require('socket.io')
 const server = express.listen(process.env.PORT);
-
 const io = sio(server);
 
+
 io.on('connect', (socket) => {
-  console.log(socket);
+  console.log('Client connected');
+  socket.emit("time", new Date());
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
