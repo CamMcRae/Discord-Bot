d440011ca093524f0513dcd27929133378e3cb1d
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-07-14 01:43:16 -0400

    dsfg

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')();
+const sio = require('socket.io')
+const server = express.listen(process.env.PORT);
+
+const io = sio.(server);
+
+io.on('connect', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
