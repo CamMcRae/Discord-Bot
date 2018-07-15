8da2dc7443d784fa0d9885ef30aad27c419c17c1
CamMcRae <33909538+CamMcRae@users.noreply.github.com>
2018-07-15 00:01:17 -0400

    dfg

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const lunch = require("./server commands/lunch.js");
 
+const http = require('https');
 const express = require('express')();
 const sio = require('socket.io')
 const server = express.listen(process.env.PORT);
 const io = sio(server);
 
+
 io.on('connect', (socket) => {
   socket.on("getMenu", async (d) => {
-    let menu = await lunch.run(d);
+    const menu = await lunch.run(d);
     socket.emit('returnMenu', menu);
   });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+setInterval(() => {
+  http.get("https://nutty-discord-bot.herokuapp.com/")
+}, 300000);
